747fc6b081ff96b4b2682703d06fccb224695888
yaseenmustapha <yaseenmust@gmail.com>
2019-10-17 04:35:54 -0700

    Added firing laser functionality

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(new Vector3(0, 1) * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 8.0)
+            Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     [SerializeField]
     private float _speed = 3.5f;
-
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.15f;
+    private float nextFire = 0.0f;
 
     // Start is called before the first frame update
     void Start() {
@@ -15,6 +19,10 @@ public class Player : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+            FireLaser();
+
     }
 
     void CalculateMovement() {
@@ -29,5 +37,10 @@ public class Player : MonoBehaviour {
             transform.position = new Vector3(-11.2f, transform.position.y);
         else if (transform.position.x < -11.2f)
             transform.position = new Vector3(11.2f, transform.position.y);
+    }
+
+    void FireLaser() {
+        nextFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f), Quaternion.identity);
     }
 }
