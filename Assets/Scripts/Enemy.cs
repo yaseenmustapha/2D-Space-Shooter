be3a40ec8dee5e2622417070720da897ece75c10
yaseenmustapha <yaseenmust@gmail.com>
2019-10-17 22:43:07 -0700

    Added enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField]
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -5.4f)
+            transform.position = new Vector3(Random.Range(-9.4f, 9.4f), 7.0f);
+    }
+}
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -8,7 +8,7 @@ public class Laser : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        transform.Translate(new Vector3(0, 1) * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
         if (transform.position.y >= 8.0)
             Destroy(gameObject);
